226129c0cd941b6366ce6879034f0f4091b55d13
dnvinicius00@gmail.com <dnvinicius00@gmail.com>
2025-03-11 12:39:40 -0300

    configurado o inventario e a home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Bem-vindo ao Sistema de Estoque!</h1>
+            <p>Gerencie os seus produtos de uma forma rápida e eficiente</p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Inventory.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Card from "../components/Card";
+
+const Inventory = () => {
+    const products = [
+        {
+            id: 1,
+            title: "Produto 1",
+            description: "Descrição do Produto 1"
+        },
+        {
+            id: 2,
+            title: "Produto 2",
+            description: "Descrição do Produto 2"
+        },
+    ]
+
+    return(
+        <div>
+            <h1>Estoque de Produtos</h1>
+            <div className="product-list">
+                {products.map((product) => (
+                    <Card key={product.id} title={product.title} description={product.description} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Inventory;
