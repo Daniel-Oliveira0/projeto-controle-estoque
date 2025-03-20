94d53fc91039378f2c0db8d89e823a876c4508d5
dnvinicius00@gmail.com <dnvinicius00@gmail.com>
2025-03-19 23:12:41 -0300

    configurado o cors

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const pool = require('./db'); 
 
 const app = express();
 const port = 5000;
 
+app.use(cors());
 app.use(express.json());
 
 
