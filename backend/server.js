c7bd2687da3a3c5264645aba9c3be8632559c45b
dnvinicius00@gmail.com <dnvinicius00@gmail.com>
2025-03-19 22:11:50 -0300

    adicionado o cadastro do produto

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,36 @@ const pool = require('./db');
 const app = express();
 const port = 5000;
 
+app.use(express.json());
+
 
 app.get('/products', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM products');
-    res.status(200).json(result.rows);  
+    res.status(200).json(result.rows);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Erro ao consultar produtos' });
+  }
+});
+
+
+app.post('/products', async (req, res) => {
+  const { title, description, quantity } = req.body;
+
+  if (!title || !description || quantity === undefined) {
+    return res.status(400).json({ message: "Título, descrição e quantidade são obrigatórios" });
+  }
+
+  try {
+    const query = 'INSERT INTO products (title, description, quantity) VALUES ($1, $2, $3) RETURNING *';
+    const values = [title, description, quantity];
+    const result = await pool.query(query, values);
+
+    res.status(201).json(result.rows[0]); 
+  } catch (err) {
+    console.error('Erro ao adicionar produto:', err);
+    res.status(500).json({ message: "Erro ao adicionar produto" });
   }
 });
 
